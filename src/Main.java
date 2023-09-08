db78601dd42ab3f1452305ff8b14f361a15e3756
Rosane da Paixão <133041345+rosanepaixao@users.noreply.github.com>
2023-09-08 09:30:08 -0300

    Permitir cadastro de sites

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("------MEUS SITES FAVORITOS ------");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
+        List<Website> meusWebsites = new ArrayList<>();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        String nome = scanner.nextLine();
+        String url = scanner.nextLine();
+
+        Website website = new Website();
+        website.setNome(nome);
+        website.setUrl(url);
+
+        meusWebsites.add(website);
+
     }
 }
